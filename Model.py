549f4f467ef57375d282c1bea1c7d0aef3044002
RazanAlkhadhiri <62849978+RazanAlkhadhiri@users.noreply.github.com>
2021-02-04 15:50:52 +0300

    Update Model.py

--- Model.py
+++ Model.py
@@ -1,8 +1,6 @@
 import torch
 from torch import nn
 import torchvision
-from torch.nn.utils.weight_norm import weight_norm
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
