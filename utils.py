24ae436be151ade9beafad5471296e93e6920f27
SarahAlhabib <62848859+SarahAlhabib@users.noreply.github.com>
2021-02-05 21:20:09 +0300

    Initial commit

---
+++ utils.py
@@ -0,0 +1,64 @@
+"""
+this code is from https://github.com/sgrvinod/a-PyTorch-Tutorial-to-Image-Captioning/blob/cc9c7e2f4017938d414178d3781fed8dbe442852/utils.py
+"""
+import torch
+
+
+def load_embeddings(emb_file, word_map):
+    """
+    Creates an embedding tensor for the specified word map, for loading into the model.
+    :param emb_file: file containing embeddings (stored in GloVe format)
+    :param word_map: word map
+    :return: embeddings in the same order as the words in the word map, dimension of embeddings
+    """
+
+
+def adjust_learning_rate(optimizer, shrink_factor):
+    """
+    Shrinks learning rate by a specified factor.
+    :param optimizer: optimizer whose learning rate must be shrunk.
+    :param shrink_factor: factor in interval (0, 1) to multiply learning rate with.
+    """
+
+    print("\nDECAYING learning rate.")
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = param_group['lr'] * shrink_factor
+    print("The new learning rate is %f\n" % (optimizer.param_groups[0]['lr'],))
+
+
+def save_checkpoint(data_name, epoch, epochs_since_improvement, decoder, decoder_optimizer, bleu4, is_best):
+
+    """
+    Saves model checkpoint.
+    :param data_name: base name of processed dataset
+    :param epoch: epoch number
+    :param epochs_since_improvement: number of epochs since last improvement in BLEU-4 score
+    :param encoder: encoder model
+    :param decoder: decoder model
+    :param encoder_optimizer: optimizer to update encoder's weights, if fine-tuning
+    :param decoder_optimizer: optimizer to update decoder's weights
+    :param bleu4: validation BLEU-4 score for this epoch
+    :param is_best: is this checkpoint the best so far?
+    """
+    state = {'epoch': epoch,
+             'epochs_since_improvement': epochs_since_improvement,
+             'bleu-4': bleu4,
+             'decoder': decoder,
+             'decoder_optimizer': decoder_optimizer}
+    filename = 'checkpoint_' + data_name + '.pth.tar'
+    torch.save(state, filename)
+    # If this checkpoint is the best so far, store a copy so it doesn't get overwritten by a worse checkpoint
+    if is_best:
+        torch.save(state, 'BEST_' + filename)
+
+
+def clip_gradient(optimizer, grad_clip):
+    """
+    Clips gradients computed during backpropagation to avoid explosion of gradients.
+    :param optimizer: optimizer with the gradients to be clipped
+    :param grad_clip: clip value
+    """
+    for group in optimizer.param_groups:
+        for param in group['params']:
+            if param.grad is not None:
+                param.grad.data.clamp_(-grad_clip, grad_clip)
