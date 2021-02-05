381047164075e42cab8c3d363af4bc89d7ee79c5
SarahAlhabib <62848859+SarahAlhabib@users.noreply.github.com>
2021-02-05 22:22:18 +0300

    Update save_checkpoint() param description

--- utils.py
+++ utils.py
@@ -33,9 +33,7 @@ def save_checkpoint(data_name, epoch, epochs_since_improvement, decoder, decoder
     :param data_name: base name of processed dataset
     :param epoch: epoch number
     :param epochs_since_improvement: number of epochs since last improvement in BLEU-4 score
-    :param encoder: encoder model
     :param decoder: decoder model
-    :param encoder_optimizer: optimizer to update encoder's weights, if fine-tuning
     :param decoder_optimizer: optimizer to update decoder's weights
     :param bleu4: validation BLEU-4 score for this epoch
     :param is_best: is this checkpoint the best so far?
