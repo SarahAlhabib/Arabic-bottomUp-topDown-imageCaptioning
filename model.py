8e14334ed4f83ab399e437d67324c1ad785c7bc1
SarahAlhabib <62848859+SarahAlhabib@users.noreply.github.com>
2021-02-05 21:23:49 +0300

    Initial commit

--- model.py
+++ model.py
@@ -1,6 +1,5 @@
 """
-this code is adaoted from https://github.com/sgrvinod/a-PyTorch-Tutorial-to-Image-Captioning/blob/cc9c7e2f4017938d414178d3781fed8dbe442852/models.py
-
+this code is adapted from https://github.com/sgrvinod/a-PyTorch-Tutorial-to-Image-Captioning/blob/cc9c7e2f4017938d414178d3781fed8dbe442852/models.py
 """
 import torch
 from torch import nn
