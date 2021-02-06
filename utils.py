835e2f19fa1c3b3bffa77c3c386b72986e44b1ae
SarahAlhabib <62848859+SarahAlhabib@users.noreply.github.com>
2021-02-06 21:05:42 +0300

    Add AverageMeter class

--- utils.py
+++ utils.py
@@ -60,3 +60,24 @@ def clip_gradient(optimizer, grad_clip):
         for param in group['params']:
             if param.grad is not None:
                 param.grad.data.clamp_(-grad_clip, grad_clip)
+
+
+class AverageMeter(object):
+    """
+    Keeps track of most recent, average, sum, and count of a metric.
+    """
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
