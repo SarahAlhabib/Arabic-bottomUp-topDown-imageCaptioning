be4527dc9070fd66395f0aaf5f89dbb64879c82e
lamaMajedd <48339233+lamaMajedd@users.noreply.github.com>
2021-02-07 00:08:46 +0300

    Update create_input_files.py

--- create_input_files.py
+++ create_input_files.py
@@ -2,9 +2,9 @@ from utils import create_input_files
 
 if __name__ == '__main__':
     # Create input files (along with word map)
-    create_input_files(dataset='coco',
-                       karpathy_json_path='data/caption_datasets/dataset_coco.json',
-                       captions_per_image=5,
+    create_input_files(dataset='flick8k',
+                       karpathy_json_path='data/caption_datasets/dataset_flick8k.json',
+                       captions_per_image=3,
                        min_word_freq=5,
                        output_folder='final_dataset',
                        max_len=50)
