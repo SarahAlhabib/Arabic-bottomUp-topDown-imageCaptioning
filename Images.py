b1e7edf3c07acc10be9992495a173e497e2e0019
SarahAlhabib <62848859+SarahAlhabib@users.noreply.github.com>
2021-02-11 22:18:21 +0300

    ..

--- Images.py
+++ Images.py
@@ -1,5 +1,5 @@
 #loading the images
-from PIL import Image
+
 from torchvision.transforms import ToTensor, ToPILImage
 import numpy as np
 import random
@@ -9,12 +9,13 @@ import io
 import os
 import pandas as pd
 
-from torch.utils.data import Dataset
 import torch
+from torch.utils.data import Dataset
+from arabic_dataset import get_captions_dic
 
 
 class Dataset(Dataset):
-    def __init__(self, txt_path='/Users/arwa/PycharmProjects/Dataset/Dataset/data/Flickr8k_text/Flickr_8k.devImages.txt ', img_dir=' /Users/arwa/PycharmProjects/Dataset/Dataset/Flicker8k_Dataset ', transform=None):
+    def __init__(self, captions_path='/Users/arwa/PycharmProjects/Dataset/Dataset/data/Flickr8k_text/Flickr_8k.devImages.txt ', features_path=' /Users/arwa/PycharmProjects/Dataset/Dataset/Flicker8k_Dataset ', split='TRAIN'):
         """
         Initialize data set as a list of IDs corresponding to each item of data set
 
@@ -22,39 +23,22 @@ class Dataset(Dataset):
         :param txt_path: a text file containing names of all of images line by line
         :param transform: apply some transforms like cropping, rotating, etc on input image
         """
+        self.split = split
+        self.captions=get_captions_dic(split)
+        self.features_path=features_path
 
-        df = pd.read_csv(txt_path, sep=' ', index_col=0)
-        self.img_names = df.index.values
-        self.txt_path = txt_path
-        self.img_dir = img_dir
-        self.transform = transform
-        self.to_tensor = ToTensor()
-        self.to_pil = ToPILImage()
-        self.get_image_selector = True if img_dir.__contains__('tar') else False
-        self.tf = tarfile.open(self.img_dir) if self.get_image_selector else None
 
-    def get_image_from_tar(self, name):
+    def get_encoded_image(self, id):
         """
-        Gets a image by a name gathered from file list csv file
+        Gets an image features by id
 
-        :param name: name of targeted image
-        :return: a PIL image
-        """
-        image = self.tf.extractfile(name)
-        image = image.read()
-        image = Image.open(io.BytesIO(image))
-        return image
-
-    def get_image_from_folder(self, name):
-        """
-        gets a image by a name gathered from file list text file
-
-        :param name: name of targeted image
-        :return: a PIL image
+        :param id: name of targeted image
+        :return: an encoded image
         """
 
-        image = Image.open(os.path.join(self.img_dir, name))
-        return image
+        encoded_image=[]
+        return encoded_image
+
 
     def __len__(self):
         """
@@ -62,7 +46,7 @@ class Dataset(Dataset):
 
         :return: number of samples in data set
         """
-        return len(self.img_names)
+        return len(self.captions.keys())
 
     def __getitem__(self, index):
         """
@@ -73,25 +57,12 @@ class Dataset(Dataset):
         :return: a sample of data as a dict
         """
 
-        if index == (self.__len__() - 1) and self.get_image_selector:  # close tarfile opened in __init__
-            self.tf.close()
+        img=[] # image features
+        caption=[]
+        caption_length=0
+        all_captions=[]
 
-        if self.get_image_selector:  # note: we prefer to extract then process!
-            X = self.get_image_from_tar(self.img_names[index])
-        else:
-            X = self.get_image_from_folder(self.img_names[index])
-
-        # Get you label here using available pandas functions
-        Y = #########
-
-        if self.transform is not None:
-            X = self.transform(X)
-#            Y = self.transform(Y) # if your label is image too - remove if it is number
-
-        sample = {'X': X,
-                  'Y': Y}
-
-        return sample
+        return img, caption, caption_length, all_captions
 
 def main():
         # DataLoader
