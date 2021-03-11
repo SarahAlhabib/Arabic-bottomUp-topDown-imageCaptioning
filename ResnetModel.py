7bef0eced3b9316ef79439b774785ca219338427
SarahAlhabib <62848859+SarahAlhabib@users.noreply.github.com>
2021-03-11 21:52:06 +0300

    Update DecoderLSTM

--- ResnetModel.py
+++ ResnetModel.py
@@ -2,44 +2,42 @@ import torch
 from torch import nn
 import torchvision
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 class EncoderResnet(nn.Module):
-    def __init__(self,  decoder_dim):
+    def __init__(self, decoder_dim):
         super(EncoderResnet, self).__init__()
 
         resnet = torchvision.models.resnet101(pretrained=True)
         for param in resnet.parameters():
             param.requires_grad_(False)
-        
+
         modules = list(resnet.children())[:-1]
         self.resnet = nn.Sequential(*modules)
         self.embed = nn.Linear(resnet.fc.in_features, decoder_dim)
 
-        
-
     def forward(self, images):
-      """
-      :param images: images, a tensor of dimensions (batch_size, 3, image_size, image_size)
-      :return: encoded images [batch_size, encoded_image_size=14, encoded_image_size=14, 2048]
-      """ 
-      features = self.resnet(images)
-      features = features.view(features.size(0), -1)
-      features = self.embed(features)
-      return features
-
+        """
+        :param images: images, a tensor of dimensions (batch_size, 3, image_size, image_size)
+        :return: encoded images [batch_size, encoded_image_size=14, encoded_image_size=14, 2048]
+        """
+        features = self.resnet(images)
+        features = features.view(features.size(0), -1)
+        features = self.embed(features)
+        return features
 
 
 class DecoderLSTM(nn.Module):
-    def __init__(self, emb_dim , decoder_dim, vocab_size, num_layers=1):
+    def __init__(self, emb_dim, decoder_dim, vocab_size, num_layers=1):
         super(DecoderLSTM, self).__init__()
 
+        self.vocab_size = vocab_size
+        self.decoder_dim = decoder_dim
         self.embedding = nn.Embedding(vocab_size, emb_dim)
-        self.lstm = nn.LSTM(input_size = emb_dim ,hidden_size = decoder_dim,
-                            num_layers = num_layers, batch_first = True)
-        
-        self.linear = nn.Linear(decoder_dim, vocab_size)
+        self.lstm = nn.LSTMCell(emb_dim, decoder_dim, bias=True)
+        self.linear = nn.Linear(decoder_dim, vocab_size).to()
+        self.dropout = nn.Dropout(p=0.5)
         self.init_weights()  # initialize some layers with the uniform distribution
 
     def init_weights(self):
@@ -74,39 +72,33 @@ class DecoderLSTM(nn.Module):
         c = torch.zeros(batch_size, self.decoder_dim).to(device)
         return h, c
 
-
     def forward(self, image_features, encoded_captions, caption_lengths):
         batch_size = image_features.size(0)
         vocab_size = self.vocab_size
 
-        # Flatten image
-        image_features_mean = image_features.mean(1).to(device)  # (batch_size, num_pixels, encoder_dim)
-        # image_features_mean = torch.flatten(image_features, start_dim=1)
-
         # Sort input data
         caption_lengths, sort_ind = caption_lengths.squeeze(1).sort(dim=0, descending=True)
         image_features = image_features[sort_ind]
-        image_features_mean = image_features_mean[sort_ind]
         encoded_captions = encoded_captions[sort_ind]
 
         # Embedding
+        self.embedding = self.embedding  # .to(device)
         embeddings = self.embedding(encoded_captions)  # (batch_size, max_caption_length, embed_dim)
 
         # Initialize LSTM state
         h1, c1 = self.init_hidden_state(batch_size)  # (batch_size, decoder_dim)
-
+        h1 = image_features
 
         # We won't decode at the <end> position, since we've finished generating as soon as we generate <end>
         decode_lengths = (caption_lengths - 1).tolist()
 
         # Create tensors to hold word predicion scores
         predictions = torch.zeros(batch_size, max(decode_lengths), vocab_size).to(device)
-        
 
         for t in range(max(decode_lengths)):
             batch_size_t = sum([l > t for l in decode_lengths])
 
-            h1, c1 = self.lstm(torch.cat(image_features_mean [:batch_size_t, t, :],embeddings[:batch_size_t, t, :], dim=1), (h1[:batch_size_t], c1[:batch_size_t]))
+            h1, c1 = self.lstm(embeddings[:batch_size_t, t, :], (h1[:batch_size_t], c1[:batch_size_t]))
             preds = self.linear(self.dropout(h1))  # (batch_size_t, vocab_size)
             predictions[:batch_size_t, t, :] = preds
 
