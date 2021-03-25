efb0c67a6759255309239685dd05f09ac78a823b
SarahAlhabib <62848859+SarahAlhabib@users.noreply.github.com>
2021-03-25 14:35:31 +0300

    Update file path

--- create_input_files.py
+++ create_input_files.py
@@ -3,4 +3,4 @@ from arabic_dataset import create_input_files
 
 if __name__ == '__main__':
     # Create input files (along with word map)
-    create_input_files("/content/‏Flickr8k.arabic.full.tsv")
+    create_input_files("Flickr8k_text/Flickr8k.arabic.full.txt")
